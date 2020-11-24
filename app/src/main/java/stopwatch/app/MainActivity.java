eacbb9645e5cd0a0ba4ba3a4ed8f7ac8439f62d1
Dominik277 <dominikdoric277@gmail.com>
2020-11-24 15:06:31 +0100

    Zavrsen vizualni dio i dodane onClick metode za Button-e.

--- app/src/main/java/stopwatch/app/MainActivity.java
+++ app/src/main/java/stopwatch/app/MainActivity.java
@@ -1,8 +1,8 @@
 package stopwatch.app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +11,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void startBrojanje(View view){
+
+    }
+
+    public void stopBrojanje(View view){
+
+    }
+
+    public void resetirajBrojanje(View view){
+
+    }
+
+
 }
